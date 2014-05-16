4802a3a47da9f6ec782d2c493a9c78fb2bfcf4ba
Max Skupin <maxskupin@gmail.com>
2014-05-15 22:35:32 -0400

    default spree with sqlite

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = 'e69b456110437d7141051ca0d3129054fdd347c5f54a8e7eacee55acce9fc75667544cadf869881542823787a567e94823794287e74b9280d1148abd6a6f3993'
